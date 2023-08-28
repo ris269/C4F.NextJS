5dd21d6045fb994da224ff735870ea4c6ae30f3d
ris269 <ristudio269@gmail.com>
2023-08-28 16:52:30 +0700

    17.Layout

--- series-nextjs/src/app/layout.tsx
+++ series-nextjs/src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <header>Header area</header>
       <body className={inter.className}>{children}</body>
+      <footer>Footer area</footer>
     </html>
   )
 }
