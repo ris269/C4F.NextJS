9f958b988d46bd95a530abf80c21f91a193b6ff0
ris269 <ristudio269@gmail.com>
2023-08-28 19:57:45 +0700

    22. modify nav bar

---
+++ series-nextjs/src/components/app.footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { text } from "stream/consumers";
+
+type Props = {};
+
+export default function AppFooter({}: Props) {
+  return (
+    <div
+      style={{ backgroundColor: "#ddd", textAlign: "center", padding: "5px 0" }}
+    >
+      NextJS Series
+    </div>
+  );
+}
